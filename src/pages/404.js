68b0ba350c933538cc854213e03c882fa0b53d59
Allan Fernandes <Fernzallan14@gmail.com>
2023-05-23 21:58:39 +0100

    404-image

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { PageNotFound } from '../styles/404.styled';
-
+import { Link } from 'gatsby';
+import { PageNotFound, HeroImage } from '../styles/404.styled';
+import Pirate from "../assets/images/404/pirate.png"
 const NotFound = () => {
   return (
-    <PageNotFound>404</PageNotFound>
+    <PageNotFound>
+        <HeroImage src={Pirate} alt="Pirate searching" />
+        <h3>No Results Found</h3>
+        <p>We couldn't find what you're searching for:
+            <Link to="/">Go back home</Link>
+        </p>
+    </PageNotFound>
   )
 }
 
--- src/styles/404.styled.js
+++ src/styles/404.styled.js
@@ -1,7 +1,17 @@
-import styled from "styled-components";
+@import url('https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@1,300&display=swap');
 
+import styled from "styled-components";
 export const PageNotFound = styled.div`
 height: 100vh;
 display: flex;
 background-color: #E7FFF8;
+margin: auto;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+`
+
+export const HeroImage = styled.img`
+max-width: 400px;
+height: fit-content;
 `
