f954d4d07a5a884da2e2d7006b3b9f6909e6e9c2
Allan Fernandes <Fernzallan14@gmail.com>
2023-05-23 20:21:24 +0100

    404-init

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,11 @@ import * as React from "react"
 export default function Home() {
   return <div>Hello gatsby!</div>
 }
+
+// export const Head = () => <title>Home Page</title>
+
+export const Head = () =>(
+  <>
+    <title>Rollen Fernandes</title>
+  </>
+)
