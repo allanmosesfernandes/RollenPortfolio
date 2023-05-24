99d9aca899be426b02c6697c2f9b67d525617270
Allan <fernzallan14@gmail.com>
2023-05-24 15:06:46 +0100

    Header-Responsive

--- src/components/navbar/navbarStyled.js
+++ src/components/navbar/navbarStyled.js
@@ -6,26 +6,46 @@ export const NavbarStyled = styled.nav`
   padding: 1rem 3rem;
   justify-content: space-between;
   align-items: center;
+
   h2 {
     font-family: ${(props) => props.theme.secondaryFont};
     color: ${(props) => props.theme.secondaryColor};
     margin: auto;
     border-bottom: 5px solid #551a8b;
-    font-size: 3rem;
+    font-size: ${(props) => props.theme.fsLG};
   }
+
   /* Media Queries for Tablets */
   @media screen and (max-width: 800px) {
     padding: 1rem 2rem;
+    h2 {
+      font-size: ${(props) => props.theme.fsMedium};
+    }
   }
+
   /* Media Queries for phone */
   @media screen and (max-width: 500px) {
     padding: 1rem;
+
+    h2 {
+      font-size: ${(props) => props.theme.fsSmall};
+    }
   }
 `;
 
 export const NavLink = styled(Link)`
-font-family: ${props => props.theme.primaryFont};
-color: inherit;
-text-decoration: none;
-letter-spacing: 1px;
-`
+  font-family: ${(props) => props.theme.primaryFont};
+  color: inherit;
+  text-decoration: none;
+  letter-spacing: 1px;
+
+  /* Media Queries for Tablets */
+  @media screen and (max-width: 800px) {
+      font-size: ${(props) => props.theme.fsSmall};
+  }
+
+  /* Media Queries for phone */
+  @media screen and (max-width: 500px) {
+      font-size: ${(props) => props.theme.fsXsmall};
+  }
+`;
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -39,7 +39,7 @@ export const theme = {
     fsXL: "5rem",
     fsLG: "4rem",
     fsMedium: "3rem",
-    fsSmall: "2rem",
+    fsSmall: "1.2rem",
     fsXsmall: "1rem",
-    
+
 }
