99cccdbb6fd83ba0b65b8d6473decfa7337a31b9
Allan <fernzallan14@gmail.com>
2023-05-24 13:04:53 +0100

    font-face

--- src/components/navbar/navbarStyled.js
+++ src/components/navbar/navbarStyled.js
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const NavbarStyled = styled.nav`
 width: 100%;
 display: flex;
+
+h2 {
+    font-family: ${props => props.theme.secondaryFont}
+}
 `
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,9 +1,12 @@
 import { createGlobalStyle } from "styled-components";
-
+import Visual from "../assets/fonts/VisueltPro-Regular.ttf"
 
 export const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300&display=swap');
-
+@font-face {
+    font-family: 'Visual';
+    src: url(${Visual}) format('truetype');
+}
 *,
 *:before,
 *:after {
@@ -12,11 +15,13 @@ export const GlobalStyle = createGlobalStyle`
 
 body {
     font-family: ${props => props.theme.primaryFont};
+    background-color: ${props => props.theme.primaryColor};
 }
 `
 
 export const theme = {
    primaryColor: "#000000",
-   secondaryColor: "#0000ff",
+   secondaryColor: "#7127BA",
    primaryFont: "'IBM Plex Mono', monospace",
+   secondaryFont: "Visual"
 }
