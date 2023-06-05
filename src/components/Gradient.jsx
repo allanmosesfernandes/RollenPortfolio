8d2d103596374d798449c63db3ce227bfbaa4520
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-05 17:07:23 +0100

    Image-assets

--- src/components/Gradient.jsx
+++ src/components/Gradient.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import styled from "styled-components"
+import Star from "../assets/images/Gradient/Star.svg";
+import Globe from "../assets/images/Gradient/Globe.svg";
+
 const GradientStyledDiv = styled.div`
-    background: white;
+background-image: linear-gradient(to right, #434343 0%, black 100%);
+width: 100%;
+height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+
 `
 const Gradient = () => {
   return (
-    <GradientStyledDiv>Gradient</GradientStyledDiv>
+    <GradientStyledDiv>
+        <h2>
+            <span><img src={Star} alt="Star"/></span>43 Designs<span><img src={Globe} alt="Globe"/></span></h2>
+    </GradientStyledDiv>
   )
 }
 
