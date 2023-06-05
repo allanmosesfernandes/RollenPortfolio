e6d18c2e7941d47622700d2172f3c31e25754a98
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-06 00:50:07 +0100

    first-fold-stars

--- src/components/Gradient.jsx
+++ src/components/Gradient.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import styled from "styled-components"
 import Star from "../assets/images/Gradient/10.png";
 import Globe from "../assets/images/Gradient/11.png";
-import eclipse from "../assets/images/Gradient/eclipse.png"
+import eclipse from "../assets/images/Gradient/d.svg";
+
 const GradientStyledDiv = styled.div`
-background-image: linear-gradient(to right, #434343 0%, black 100%);
+background-image: linear-gradient(to right, black 0%, #434343 100%);
 width: 100%;
 height: 100vh;
 display: flex;
@@ -12,7 +13,21 @@ flex-direction: column;
 align-items: center;
 justify-content: center;
 font-family: ${(props) => props.theme.nymphaFont};
-
+perspective: 1000px;
+.rotating-image {
+  width: 100px;
+  height: 100px;
+  transform-style: preserve-3d; /* Ensures the child elements maintain their 3D position */
+  animation: rotate 5s infinite linear; /* Animation for continuous rotation */
+}
+@keyframes rotate {
+  from {
+    transform: rotateY(0deg); /* Initial rotation angle */
+  }
+  to {
+    transform: rotateY(360deg); /* Final rotation angle */
+  }
+}
 h2 {
     display: flex;
     align-items: center;
@@ -20,14 +35,22 @@ h2 {
     letter-spacing:3px;
     font-size: 8rem;
     background-image: url(${eclipse});
+
+    .star {
+        height: 100%;
+        display: flex;
+        margin: 0 1rem;
+    }
 }
 
 `
 const Gradient = () => {
   return (
     <GradientStyledDiv>
+        <img src={Globe} alt="rotating" className='rotating-image'/>
         <h2>
-            <span><img src={Star} alt="Star"/></span>43 Designs<span><img src={Globe} alt="Globe"/></span></h2>
+            <span><img src={Star} alt="Star" className="star" /></span> 43 Designs <span><img src={Star} alt="Globe" className="star"/></span>
+        </h2>
     </GradientStyledDiv>
   )
 }
