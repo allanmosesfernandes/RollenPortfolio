2ea4e0d4096d65e729d38d7836e4eb7d37e0fba0
Allan <fernzallan14@gmail.com>
2023-06-06 11:07:58 +0100

    eclipse-perfect

--- src/components/Gradient.jsx
+++ src/components/Gradient.jsx
@@ -2,57 +2,72 @@ import React from 'react'
 import styled from "styled-components"
 import Star from "../assets/images/Gradient/10.png";
 import Globe from "../assets/images/Gradient/11.png";
-import eclipse from "../assets/images/Gradient/d.svg";
+import eclipse from "../assets/images/Gradient/a1.svg";
 
 const GradientStyledDiv = styled.div`
-background-image: linear-gradient(to right, black 0%, #434343 100%);
-width: 100%;
-height: 100vh;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-font-family: ${(props) => props.theme.nymphaFont};
-perspective: 1000px;
-.rotating-image {
-  width: 100px;
-  height: 100px;
-  transform-style: preserve-3d; /* Ensures the child elements maintain their 3D position */
-  animation: rotate 5s infinite linear; /* Animation for continuous rotation */
-}
-@keyframes rotate {
-  from {
-    transform: rotateY(0deg); /* Initial rotation angle */
+  background-image: linear-gradient(to right, black 0%, #434343 100%);
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: ${(props) => props.theme.nymphaFont};
+  perspective: 1000px;
+  .rotating-image {
+    width: 100px;
+    height: 100px;
+    transform-style: preserve-3d; /* Ensures the child elements maintain their 3D position */
+    animation: rotate 5s infinite linear; /* Animation for continuous rotation */
   }
-  to {
-    transform: rotateY(360deg); /* Final rotation angle */
+  @keyframes rotate {
+    from {
+      transform: rotateY(0deg); /* Initial rotation angle */
+    }
+    to {
+      transform: rotateY(360deg); /* Final rotation angle */
+    }
   }
-}
-h2 {
+  h2 {
     display: flex;
     align-items: center;
     justify-content: center;
-    letter-spacing:3px;
+    letter-spacing: 3px;
     font-size: 8rem;
-    background-image: url(${eclipse});
 
     .star {
-        height: 100%;
-        display: flex;
-        margin: 0 1rem;
+      height: 100%;
+      display: flex;
+      margin: 0 1rem;
     }
-}
-
-`
+  }
+  .eclipseDiv {
+    background-image: url(${eclipse});
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: center;
+  }
+`;
 const Gradient = () => {
   return (
     <GradientStyledDiv>
-        <img src={Globe} alt="rotating" className='rotating-image'/>
+      <div className="eclipseDiv">
         <h2>
-            <span><img src={Star} alt="Star" className="star" /></span> 43 Designs <span><img src={Star} alt="Globe" className="star"/></span>
+          <span>
+            <img src={Star} alt="Star" className="star" />
+          </span>{" "}
+          43 Designs{" "}
+          <span>
+            <img src={Star} alt="Globe" className="star" />
+          </span>
         </h2>
+      </div>
     </GradientStyledDiv>
-  )
+  );
 }
 
 export default Gradient
